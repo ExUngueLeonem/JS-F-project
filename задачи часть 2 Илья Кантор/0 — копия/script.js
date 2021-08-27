bbe96b434ce3150d7acc93667f1c2b5d456022ea
ExUngueLeonem <ujfox13@gmail.com>
2021-08-27 18:56:52 +0300

    прохождение курса Udemy

--- задачи часть 2 Илья Кантор/0 — копия/script.js
+++ задачи часть 2 Илья Кантор/0 — копия/script.js
@@ -1,4 +1,5 @@
-'use strict'
-console.log(null >= 0);
-console.log(null <= 0);
-console.log('hello world in app')
+for(let i=0; i<10; i++) {
+    setTimeout( () => {
+      alert(i);
+    }, 100);
+  }
