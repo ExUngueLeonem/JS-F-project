68525cba5487224b5815e4e336ec09d2b6ab6ade
Leo <ujfox13@gmail.com>
2021-08-29 23:35:17 +0300

    Вопросы к собеседованию и объекты Udemy

--- Курс Udemy/01 Практика - Основы JS/4 Используем объекты/script.js
+++ Курс Udemy/01 Практика - Основы JS/4 Используем объекты/script.js
@@ -14,4 +14,87 @@
 
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
+const personalMovieDb = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+
+    start: function () {
+        this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+        while (this.count == '' || this.count == null || isNaN(this.count)) {
+            this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+
+    detectPersonaLevel: function () {
+        (this.count < 10) ? console.log('Просмотрено довольно мало фильмов'):
+            (this.count >= 10 && this.count < 30) ? console.log('Вы классический зритель') :
+            (this.count >= 30 && this.count < 50) ? console.log('Вы смотрите очень много фильмов') :
+            (this.count >= 50) ? console.log('Вы киноман') : console.log('Сложно было ввести нормально?');
+    },
+
+    rememberMyFilms: function () {
+
+        for (let i = 0; i < this.count; i++) {
+            let film = prompt('Один из последних просмотренных фильмов', '');
+            let ladder = prompt('На сколько оцените его?', '');
+
+            if (film != null && ladder != null &&
+                film != '' && ladder != '' && film.length < 50) {
+                this.movies[film] = ladder;
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+
+    showMyDB: function () {
+        if (!this.private) {
+            console.log(this);
+        }
+    },
+
+    writeYourGenres: function () {
+        let genre;
+
+            genre = prompt(`Ваши любимые жанры кинофильмов (через запятую)`, '');
+
+            if (genre != '' && genre != null) {
+                this.genres = genre.split(', ');
+            } else {
+                console.log('введите нормально');
+            }
+        
+        this.genres.forEach( function(genre, i) {
+            console.log(`Любимый жанр номер ${i + 1 } это ${genre}`);
+            
+        });
+
+
+    },
+
+    toggleVisibleMyDB: function() {
+
+        switch (this.private) {
+            case true:
+                this.private = false;
+                break;
+            default:
+                this.private = true;
+                break;
+        }
+
+    }
+
+};
+
+//personalMovieDb.start();
+//personalMovieDb.detectPersonaLevel();
+//personalMovieDb.rememberMyFilms();
+personalMovieDb.writeYourGenres();
+//personalMovieDb.toggleVisibleMyDB();
+personalMovieDb.showMyDB();
