630b0d9e6c42deca2312b46f956f911e7f32867b
Leo <ujfox13@gmail.com>
2021-08-31 00:20:00 +0300

    Отработка действий со страницей. Udemy

---
+++ Курс Udemy/01 Практика - Основы JS/6 Действия со страницей/project/js/script.js
@@ -0,0 +1,63 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан", 
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Горячие хохлушки 7",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+
+document.querySelectorAll('.promo__adv img').forEach((elem) => elem.remove());
+
+document.querySelector('.promo__genre').textContent = 'драма';
+
+let promoBg = document.querySelector('.promo__bg');
+
+let filmList = document.querySelector('.promo__interactive-list');
+
+promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+
+filmList.innerHTML = '';
+console.log(filmList);
+movieDB.movies.sort();
+
+movieDB.movies.forEach( (film, i) => {
+     filmList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}) ${film}
+            <div class="delete"></div>
+        </li>
+    `; 
+    console.log(i);
+});
+
+
+/* 
+for (let i = 0; i < filmList.length; i++) {
+    filmList[i].innerHTML = movieDB.movies[i]
+}
+
+
+for (let i = 0; i < filmList.length; i++) {
+    filmList[i].innerHTML = i + 1 + ') ' + filmList[i].innerHTML;
+}
+ */
+
+
