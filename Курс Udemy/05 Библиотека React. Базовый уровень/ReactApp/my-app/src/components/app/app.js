92c6f82df439976117137cf11f3c7d4784f72cfb
Leo <ujfox13@gmail.com>
2021-09-26 16:19:23 +0300

    Улучшаем приложение. Назначаем обработчики событий.
    Урок - свойства и состояния компонентов

--- Курс Udemy/05 Библиотека React. Базовый уровень/ReactApp/my-app/src/components/app/app.js
+++ Курс Udemy/05 Библиотека React. Базовый уровень/ReactApp/my-app/src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'dsfg'},
+        {label: 'That is so good', important: false, id: 'jdfhg'},
+        {label: 'I need a break', important: false, id: 'jafhj'}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
