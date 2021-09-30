5f463e5583017fc7a47701ecd30c714b1fd73d85
Leo <ujfox13@gmail.com>
2021-09-30 23:14:04 +0300

    Liked & Important

--- Курс Udemy/05 Библиотека React. Базовый уровень/ReactApp/my-app/src/components/app/app.js
+++ Курс Udemy/05 Библиотека React. Базовый уровень/ReactApp/my-app/src/components/app/app.js
@@ -58,7 +58,18 @@ export default class App extends Component {
     }
 
     onToggleImportant(id) {
-        console.log('important', id);
+        this.setState(({data}) => {
+            const index = data.findIndex( elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, important: !old.important};
+
+            const newArr = [...data.slice(0, index), newItem,...data.slice(index + 1)]
+        
+            return {
+                data: newArr
+            }
+        })
     }
 
     onToggleLiked(id) {
@@ -77,9 +88,15 @@ export default class App extends Component {
     }
 
     render () {
+        const {data} = this.state
+        const liked = data.filter( item => item.like).length;
+        const allPosts = data.length;
+
         return (
             <div className="app">
-                <AppHeader/>
+                <AppHeader
+                liked={liked}
+                allPosts={allPosts}/>
                 <div className="search-panel d-flex">
                     <SearchPanel/>
                     <PostStatusFilter/>
