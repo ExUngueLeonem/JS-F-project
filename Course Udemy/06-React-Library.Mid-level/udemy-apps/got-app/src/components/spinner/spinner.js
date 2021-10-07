d29f7afe93f3e54d0b2ec42319f6c5de3ba2d90f
Leo <ujfox13@gmail.com>
2021-10-07 22:04:25 +0300

    spinner fix

---
+++ Course Udemy/06-React-Library.Mid-level/udemy-apps/got-app/src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-ball-mkq1zm5wrj"><div class="ldio-3fjtcrr96px">
+            <div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
+
+
