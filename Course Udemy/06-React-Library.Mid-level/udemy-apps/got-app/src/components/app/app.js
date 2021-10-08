b95e7b7a6a5ab24dcc26c4eff51a8e00a35b614f
Leo <ujfox13@gmail.com>
2021-10-08 19:52:31 +0300

    gotApp HideRandomCharacter Button

--- Course Udemy/06-React-Library.Mid-level/udemy-apps/got-app/src/components/app/app.js
+++ Course Udemy/06-React-Library.Mid-level/udemy-apps/got-app/src/components/app/app.js
@@ -1,34 +1,68 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, { Component } from 'react';
+import {Col, Row, Container, Button} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            showRandomChar: true
+        }
+        
+        this.onToggleHideRandomChar = this.onToggleHideRandomChar.bind(this);
+        this.randomCharRender = this.randomCharRender.bind(this);
+
+    }
+
+    onToggleHideRandomChar(stateValue) {
+        this.setState({showRandomChar: !stateValue});
+    }
+
+    randomCharRender() {
+        let res = <Button color="info" 
+                          onClick={
+                              () => this.onToggleHideRandomChar(this.state.showRandomChar)}>не тыкай сюда</Button>
+
+        if (this.state.showRandomChar) {
+            return (
+                <>
+                    <RandomChar/> 
+                    {res}
+                </>
+            );
+        } else return res;
+    }
+
+    render () {
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {this.randomCharRender()}
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
 
 export default App;
