c763245f8b26b89512e945844289beb9d624286f
Leo <ujfox13@gmail.com>
2021-11-09 22:34:34 +0300

    111 lesson. redux

--- Course Udemy/05 React Library. Base/109 resto/src/services/resto-service.js
+++ Course Udemy/05 React Library. Base/109 resto/src/services/resto-service.js
@@ -3,7 +3,9 @@ export default class RestoService {
         let menuTitle =[];
         await fetch('http://localhost:3000/menu')
             .then(res => res.json())
-            .then(res => {return menuTitle = res.map(obj => obj.title)});
+            //.then(res => {return menuTitle = res.map(obj => obj.title)})
+            .then(res => {return menuTitle = res})
+            .then(res => console.log(res))
         return menuTitle;
     }
 }
