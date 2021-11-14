17344e8e2994c6567155069346f17cea1886e2a1
Leo <ujfox13@gmail.com>
2021-11-14 14:48:02 +0300

    react redux work

--- Course Udemy/05 React Library. Base/109 resto/src/reducers/index.js
+++ Course Udemy/05 React Library. Base/109 resto/src/reducers/index.js
@@ -1,6 +1,22 @@
 const initialState = {
     menu: [],
-    loading: true
+    loading: true,
+    items: [		
+    {
+        "title": "Greece salad",
+        "price": 8,
+        "url": "https://assets.epicurious.com/photos/576454fb42e4a5ed66d1df6b/master/pass/greek-salad.jpg",
+        "category": "salads",
+        "id": 4
+    },
+    {
+        "title": "Cowboy Steak",
+        "price": 25,
+        "url": "https://i.cbc.ca/1.4491288.1516208229!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/cowboysteak.jpg",
+        "category": "meat",
+        "id": 5
+    }
+]
 }
 
 const reducer = (state = initialState, action) => {
@@ -8,11 +24,13 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'MENU_LOADED':
             return {
+                ...state,
                 menu: action.payload,
                 loading: false
             };
         case 'MENU_REQUESTED':
             return {
+                ...state,
                 menu: state.menu,
                 loading: true
             };
